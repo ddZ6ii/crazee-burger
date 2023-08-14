7660be944c1ca0fbe02683e27d98ba337bdae5da
Denis Dezest <denis.dezest@gmail.com>
2023-08-14 07:34:06 +0200

    ✨ F02: APP > Add dynamic routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
+import { Routes, Route } from 'react-router-dom';
+
+import RootLayout from './components/layouts/RootLayout';
 import LoginPage from './components/pages/login/LoginPage';
+import OrderPage from './components/pages/order/OrderPage';
+import ErrorPage from './components/pages/error/ErrorPage';
+
 import './App.css';
 
 export default function App() {
   return (
     <>
-      <LoginPage />
+      <Routes>
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<LoginPage />} />
+          <Route path="order" element={<OrderPage />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import UserContextProvider from './contexts/UserContextProvider.jsx';
 import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </Router>
   </React.StrictMode>
 );

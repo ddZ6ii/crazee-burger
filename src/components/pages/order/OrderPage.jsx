211cd889559d66caab1295e6a4c9da1a966c3607
Denis Dezest <denis.dezest@gmail.com>
2023-08-26 11:41:35 +0200

    ♻️ 💄 F04: ORDERPAGE > Refactor code and styling for the OrderPage
    Details:
     - transfer user info and log out button code to the dedicated RootLayout > Navbar component
     - add styling to match the wirefrane

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -1,22 +1,38 @@
-import { useNavigate } from 'react-router-dom';
-
-import useUserContext from '../../../hooks/useUserContext';
+import styled from 'styled-components';
+import { theme } from '../../../themes';
 
 export default function OrderPage() {
-  const navigate = useNavigate();
-  const { userName, logOutUser } = useUserContext();
-
-  const handleClick = () => {
-    logOutUser();
-    navigate('/');
-  };
-
   return (
-    <>
-      <h1>Hello {userName}</h1>
-      <button type="button" onClick={handleClick}>
-        Log out
-      </button>
-    </>
+    <SectionStyled>
+      <h1>Order Page</h1>
+    </SectionStyled>
   );
 }
+
+/* __________________________________________________________________________ *\
+ ** Style
+/* __________________________________________________________________________ */
+const NAVBAR_HEIGHT_MOBILE = '95px';
+const NAVBAR_HEIGHT_DESKTOP = '148px';
+const { breakpoints, borderRadius, colors, fonts, spacing } = theme;
+
+const SectionStyled = styled.section`
+  padding: ${spacing.md};
+  min-height: calc(100dvh - ${NAVBAR_HEIGHT_MOBILE});
+
+  @media screen and (min-width: ${breakpoints.sm}) {
+    padding: ${spacing['2xl']} ${spacing['xl']};
+    min-height: calc(100dvh - ${NAVBAR_HEIGHT_DESKTOP});
+  }
+
+  position: relative;
+
+  background-color: ${colors.neutral_lightest};
+  border-bottom-left-radius: ${borderRadius['rounded_2xl']};
+  border-bottom-right-radius: ${borderRadius['rounded_2xl']};
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+
+  & h1 {
+    font-size: clamp(${fonts.size.xl}, 6vw, ${fonts.size['2xl']});
+  }
+`;

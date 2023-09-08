9f344514f3a233f27e99ef02ad2df2eaa0165dea
Denis Dezest <denis.dezest@gmail.com>
2023-09-08 08:33:58 +0200

    🔄 F05: UTILITIES > Update price format utility function

--- src/utilities/maths.js
+++ src/utilities/maths.js
@@ -1,18 +1,19 @@
-export function formatPrice(priceToFormat) {
-  let price = priceToFormat
-
-  // @TODO: perhaps change this to if(!price) return 0
-  if (!price) return "0,00 €"
-  price = replaceFrenchCommaWithDot(price)
-
-  const formattedPrice = new Intl.NumberFormat("fr-FR", {
-    style: "currency",
-    currency: "EUR",
-  }).format(price)
-  return formattedPrice
+function replaceFrenchCommaWithDot(price) {
+  if (typeof price === 'string') price = parseFloat(price.replace(',', '.'));
+  return price;
 }
 
-export function replaceFrenchCommaWithDot(price) {
-  if (typeof price === "string") price = parseFloat(price.replace(",", "."))
-  return price
+export function formatPrice(priceToFormat) {
+  let price = priceToFormat;
+
+  if (!price) return '0,00 €';
+
+  price = replaceFrenchCommaWithDot(price);
+
+  const formattedPrice = new Intl.NumberFormat('fr-FR', {
+    style: 'currency',
+    currency: 'EUR',
+  }).format(price);
+
+  return formattedPrice;
 }

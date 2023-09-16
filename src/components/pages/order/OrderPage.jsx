c37f2cc4eba8a3ba1592b9f8102ee4436169a117
Denis Dezest <denis.dezest@gmail.com>
2023-09-16 14:24:42 +0200

    📱 F07: ORDERPAGE > Improve responsive (smartphone, orientation)

--- src/components/pages/order/OrderPage.jsx
+++ src/components/pages/order/OrderPage.jsx
@@ -44,48 +44,46 @@ const NAVBAR_HEIGHT_DESKTOP = '148px';
 const { breakpoints, borderRadius, colors, spacing } = theme;
 
 const SectionStyled = styled.section`
-  // IN PROGRESS
-  height: calc(100dvh - ${NAVBAR_HEIGHT_DESKTOP});
-  display: grid;
-  grid-template-rows: 1fr auto;
-  /* overflow: hidden; */
-
   background-color: ${colors.neutral_lightest};
   border-bottom-left-radius: ${borderRadius['rounded_2xl']};
   border-bottom-right-radius: ${borderRadius['rounded_2xl']};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+
+  @media screen and (min-width: ${breakpoints.sm}) {
+    height: calc(100dvh - ${NAVBAR_HEIGHT_DESKTOP});
+    display: grid;
+    grid-template-rows: 1fr auto;
+  }
+  @media screen and (max-width: ${breakpoints.lg}) and (orientation: landscape) {
+    height: auto;
+    display: block;
+  }
 `;
 
 const MenuStyled = styled.div`
   padding: ${spacing.md};
-  // IN PROGRESS
-  /* min-height: calc(100dvh - ${NAVBAR_HEIGHT_MOBILE}); */
-  /* position: relative; */
+  min-height: calc(100dvh - ${NAVBAR_HEIGHT_MOBILE});
 
   display: grid;
   grid-template-columns: minmax(240px, 1fr);
   justify-items: center;
   column-gap: 60px;
   row-gap: 32px;
-
   overflow-y: scroll;
 
   @media screen and (min-width: ${breakpoints.sm}) {
     padding: ${spacing['2xl']} ${spacing['xl']};
-    // IN PROGRESS
-    /* min-height: calc(100dvh - ${NAVBAR_HEIGHT_DESKTOP}); */
+    min-height: auto;
 
     grid-template-columns: repeat(auto-fill, 240px);
     justify-content: center;
     column-gap: clamp(20px, 6vw, 85px);
     row-gap: clamp(20px, 6vw, 85px);
   }
-
   @media screen and (min-width: ${breakpoints.lg}) {
     column-gap: 85px;
     row-gap: 60px;
   }
-
   @media screen and (max-width: ${breakpoints.lg}) and (orientation: landscape) {
     padding: ${spacing.sm};
   }

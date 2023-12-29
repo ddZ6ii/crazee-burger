60a8c1794e337c2900c072c7e27ea27d8583bc60
Denis Dezest <denis.dezest@gmail.com>
2023-12-29 14:20:58 +0100

    ✨ F09: COMMON > Loader > Add variants to component styling

--- src/components/common/Loader.jsx
+++ src/components/common/Loader.jsx
@@ -1,10 +1,15 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { theme } from '../../themes';
 
-export default function Loader({ message, className, spinnerColor }) {
+export default function Loader({
+  message,
+  className,
+  version = '',
+  size = '',
+}) {
   return (
-    <LoaderStyled className={className} spinnerColor={spinnerColor}>
-      <div className="spinningLoader" />
+    <LoaderStyled className={className}>
+      <SpinnerStyled version={version} size={size} />
       {message && <p>{message}</p>}
     </LoaderStyled>
   );
@@ -19,15 +24,74 @@ const LoaderStyled = styled.div`
   font-size: ${fonts.size['xl']};
   font-family: ${fonts.family.headings};
 
-  .spinningLoader {
+  /* .spinningLoader {
     width: 24px;
     height: 24px;
     border: 3px solid ${colors.neutral_light};
-    border-top: 3px solid
-      ${(props) => (props.spinnerColor ? props.spinnerColor : colors.accent)};
+    border-top: 3px solid ${colors.accent};
     border-radius: ${borderRadius.rounded_full};
     animation: spin 1.2s linear infinite;
+  } */
+
+  /* @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  } */
+
+  @media screen and (min-width: ${breakpoints.md}) {
+    font-size: ${fonts.size['2xl']};
+    /* .spinningLoader {
+      width: 32px;
+      height: 32px;
+    } */
   }
+
+  @media screen and (orientation: landscape) and (max-width: ${breakpoints.lg}) {
+    font-size: ${fonts.size['xl']};
+    /* .spinningLoader {
+      width: 24px;
+      height: 24px;
+    } */
+  }
+`;
+
+const VARIANT_VERSIONS = {
+  accentOnWhite: css`
+    border-color: ${colors.white};
+    border-top-color: ${colors.accent};
+  `,
+};
+
+const VARIANT_SIZES = {
+  sm: css`
+    width: ${fonts.size.sm};
+    height: ${fonts.size.sm};
+    border-width: 2px;
+  `,
+  xl: css`
+    width: ${fonts.size.xl};
+    height: ${fonts.size.xl};
+    border-width: 3px;
+  `,
+  '2xl': css`
+    width: ${fonts.size['2xl']};
+    height: ${fonts.size['2xl']};
+    border-width: 3px;
+  `,
+};
+
+const SpinnerStyled = styled.div`
+  width: 24px;
+  height: 24px;
+  border: 3px solid ${colors.neutral_light};
+  border-top: 3px solid ${colors.accent};
+
+  border-radius: ${borderRadius.rounded_full};
+  animation: spin 1.2s linear infinite;
 
   @keyframes spin {
     from {
@@ -38,19 +102,19 @@ const LoaderStyled = styled.div`
     }
   }
 
+  /* Variants styling (add to or overwrite base styling) */
+  ${({ version }) => VARIANT_VERSIONS[version]}
+  ${({ size }) => VARIANT_SIZES[size]} 
+  
   @media screen and (min-width: ${breakpoints.md}) {
-    font-size: ${fonts.size['2xl']};
-    .spinningLoader {
-      width: 32px;
-      height: 32px;
-    }
+    width: 32px;
+    height: 32px;
+    ${({ size }) => VARIANT_SIZES[size]}
   }
 
   @media screen and (orientation: landscape) and (max-width: ${breakpoints.lg}) {
-    font-size: ${fonts.size['xl']};
-    .spinningLoader {
-      width: 24px;
-      height: 24px;
-    }
+    width: 24px;
+    height: 24px;
+    ${({ size }) => VARIANT_SIZES[size]}
   }
 `;
